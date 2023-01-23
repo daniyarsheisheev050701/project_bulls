563889dd91e73146e0d6aa7180a7f2c377260844
daniyarsheisheev050701 <sheisheevdaniyar@gmail.com>
2023-01-24 05:54:59 +0600

    afa

---
+++ src/componets/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Machenss from './Machen'
+import Menu from './Menu.jsx'
+
+
+const Header = () => {
+  return (
+    <div>
+      <Menu /> 
+    
+  
+    </div>
+  )
+}
+
+export default Header
